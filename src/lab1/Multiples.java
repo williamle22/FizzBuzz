df0c1018ab79ee61b24a2c532d6f28c46a5c7025
williamle <william.le@mail.utoronto.ca>
2026-09-14 12:46:07 -0400

    Done extra task 1.2

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,7 +2,17 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1000, 3, 5));
+        System.out.println(multiples());
+    }
+
+    static int multiples(){
+        int count = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     static int multiples(int n, int a, int b) {
