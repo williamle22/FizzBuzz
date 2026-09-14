ae44f866621541b7c0f87fbcaf89d24752091eec
williamle <william.le@mail.utoronto.ca>
2026-09-14 12:06:27 -0400

    Done Task 3.3

---
+++ src/lab1/Main.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+
+        // hint: a class can only have one main method so which main is it
+        String[] test = new String[]{"1", "2", "3"};
+        FizzBuzz.main(test);
+    }
+}
+
+
